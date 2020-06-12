0706f4d28db47505db044f2d792612ba98495608
Александр <medvedev.sasha2000@mail.ru>
2020-06-12 13:35:16 +0300

    Add routing for films

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FilmListComponent } from './film-list/film-list.component';
+import { FilmPageComponent } from './film-page/film-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'films', component: FilmListComponent },
+  { path: 'films/:id', component: FilmPageComponent }
+
+  // { path: 'humans', component: HumanListComponent },
+  // { path: 'genres', component: GenreListComponent },
+  // { path: 'careers', component: CareerListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
