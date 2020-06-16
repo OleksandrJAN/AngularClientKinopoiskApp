cb46261a3d9ef416fe0e53923fd890fe90a5cf7f
Александр <medvedev.sasha2000@mail.ru>
2020-06-16 12:25:21 +0300

    Add human routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { FilmListComponent } from './film-list/film-list.component';
 import { FilmPageComponent } from './film-page/film-page.component';
+import { HumanListComponent } from './human-list/human-list.component';
+import { HumanPageComponent } from './human-page/human-page.component';
 
 
 const routes: Routes = [
   { path: 'films', component: FilmListComponent },
-  { path: 'films/:id', component: FilmPageComponent }
+  { path: 'films/:id', component: FilmPageComponent },
 
-  // { path: 'humans', component: HumanListComponent },
+  { path: 'humans', component: HumanListComponent },
+  { path: 'humans/:id', component: HumanPageComponent }
+
   // { path: 'genres', component: GenreListComponent },
   // { path: 'careers', component: CareerListComponent }
 ];
