a6cfe88c5f87ad72a509cda01b7c2d428ad3cdbc
Александр <medvedev.sasha2000@mail.ru>
2020-06-23 14:57:20 +0300

    Remove useless imports

--- src/app/service/genre.service.ts
+++ src/app/service/genre.service.ts
@@ -2,8 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-import { Genre } from '../domain/genre';
-
 @Injectable({
   providedIn: 'root'
 })
