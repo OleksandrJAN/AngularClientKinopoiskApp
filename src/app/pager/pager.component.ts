d602fb1a14191b3c41d5af2b68b35ce5394a0d47
Александр <medvedev.sasha2000@mail.ru>
2020-06-27 00:06:25 +0300

    Change pager buttons range

--- src/app/pager/pager.component.ts
+++ src/app/pager/pager.component.ts
@@ -20,7 +20,39 @@ export class PagerComponent implements OnChanges {
   }
 
   setPageInfo(): void {
-    this.totalPagesRange = Array.from(Array(this.page.totalPages).keys());
+    if (this.page.totalPages > 5) {
+      // @TODO refactor
+      let lastPageIndex = this.page.totalPages - 1;
+      let currentPageIndex = this.page.number;
+
+      let pagesBeforeCurrent: number[], pagesAfterCurrent: number[];
+      if (currentPageIndex > 1 && currentPageIndex < lastPageIndex - 1) {
+        pagesBeforeCurrent = [currentPageIndex - 2, currentPageIndex - 1];
+        pagesAfterCurrent = [currentPageIndex + 1, currentPageIndex + 2];
+      }
+
+      if (currentPageIndex === 1) {
+        pagesBeforeCurrent = [currentPageIndex - 1];
+        pagesAfterCurrent = [currentPageIndex + 1, currentPageIndex + 2, currentPageIndex + 3];
+      }
+      if (currentPageIndex === 0) {
+        pagesBeforeCurrent = [];
+        pagesAfterCurrent = [currentPageIndex + 1, currentPageIndex + 2, currentPageIndex + 3, currentPageIndex + 4];
+      }
+
+      if (currentPageIndex === lastPageIndex - 1) {
+        pagesBeforeCurrent = [currentPageIndex - 3, currentPageIndex - 2, currentPageIndex - 1];
+        pagesAfterCurrent = [currentPageIndex + 1];
+      }
+      if (currentPageIndex === lastPageIndex) {
+        pagesBeforeCurrent = [currentPageIndex - 4, currentPageIndex - 3, currentPageIndex - 2, currentPageIndex - 1];
+        pagesAfterCurrent = [];
+      }
+
+      this.totalPagesRange = pagesBeforeCurrent.concat(currentPageIndex).concat(pagesAfterCurrent);
+    } else {
+      this.totalPagesRange = Array.from(Array(this.page.totalPages).keys());
+    }
     this.currentPageIndex = this.page.number;
   }
 
